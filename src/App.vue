f53cd7eb913437a2f106ed63ee331d8eca62b6f4
genospikey <jeremyreyes@gmail.com>
2024-06-14 15:20:54 -0500

    Updated favicon

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@ import data from "./assets/data.json"
 </script>
 
 <template>
-  <div class="bg-[#9996ED] w-full text-center py-3 ">
+  <div class="w-full text-center py-3 ">
     <div class="text-6xl font-extrabold mb-2">Miscellaneous Creativity</div>
     <div>
       Complaints and feedback can be addressed to <a href="mailto:jeremyreyes@gmail.com" class="underline">jeremyreyes@gmail.com</a>.
     </div>
   </div>
-  <div class="bg-[#9996ED] w-full bg-gray-600 flex flex-col items-center">
+  <div class="w-full flex flex-col items-center">
     <div v-for="i in data.entries.slice().reverse()">
       <Entry :prompt=i.prompt :text=i.text :date=i.date :author=i.author />
     </div>
