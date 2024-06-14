1e55f449c3d87108a5f64e298b6a80d767f5630e
genospikey <jeremyreyes@gmail.com>
2024-06-14 12:15:50 -0500

    OK

---
+++ src/components/Entry.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps({
+    prompt: String,
+    text: String
+})
+</script>
+
+<template>
+<div>
+    <div>{{prompt}}</div>
+    <div>{{text}}</div>
+</div>
+</template>
