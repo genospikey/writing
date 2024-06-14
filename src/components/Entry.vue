7614148736b7a55f7880c87cf3559188838e635f
genospikey <jeremyreyes@gmail.com>
2024-06-14 15:01:02 -0500

    Added source files and updated a bunch of CSS

--- src/components/Entry.vue
+++ src/components/Entry.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 const props = defineProps({
     prompt: String,
-    text: String
+    text: Array<String>,
+    author: String,
+    date: String
 })
 </script>
 
 <template>
-<div>
-    <div>{{prompt}}</div>
-    <div>{{text}}</div>
+<div class="bg-white p-3 w-[500px] m-5 rounded-xl">
+    <div class="bg-[#D1AC00] rounded-xl p-5">
+        <div class="text-sm"> Prompt: </div>
+        <div class="text-xl italic font-bold">{{prompt}}</div>
+    </div>
+    <div class="p-3">
+        <div v-for="t in text" class="mb-5 mt-2 text-xl">{{t}}</div>
+    </div>
+    <div class="p-3 flex text-sm">
+        <span class="w-1/2">By: {{author}}</span>
+        <span class="w-1/2 text-right">Date: {{date}}</span>
+    </div>
 </div>
 </template>
